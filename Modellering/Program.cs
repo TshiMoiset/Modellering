91a016cd76b0807fa1a667ec0461a237ff36cff3
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-08-30 12:24:04 +0200

    First commit

---
+++ Modellering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Modellering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("About a week ago, week ago");
+        }
+    }
+}
